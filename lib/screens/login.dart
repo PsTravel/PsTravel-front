50f8ff45b0e3a8ef3b9f3f33cc08715e5d3841a1
bolonge <qhsghd5357@gmail.com>
2023-05-13 00:48:04 +0900

    add default screens

---
+++ lib/screens/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
