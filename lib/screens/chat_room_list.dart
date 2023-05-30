9c2b6117d8d98d2459c62fe0dbbc3a11c1b5cc83
bolonge <qhsghd5357@gmail.com>
2023-05-30 22:06:49 +0900

    add 채팅룸 리스트 화면 구성

--- lib/screens/chat_room_list.dart
+++ lib/screens/chat_room_list.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ChatRoomList extends StatelessWidget {
   const ChatRoomList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("chat room list"),
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: ListView.separated(
+          itemCount: 11,
+          itemBuilder: (context, index) {
+            return Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: const SizedBox(
+                            width: 50,
+                            height: 50,
+                          ),
+                        ),
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Name"),
+                          Text("contents"),
+                        ],
+                      )
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 15),
+                    child: Icon(Icons.more_vert_sharp),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(
+            height: 50,
+          ),
+        ),
+      ),
     );
   }
 }
